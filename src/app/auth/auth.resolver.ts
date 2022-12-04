425cb4c2da78f81cbeb6d9317dd45bd928a33b58
Amogh Rijal <103477876+xamogh@users.noreply.github.com>
2022-12-04 11:47:03 +0545

    Removed throttling until serverless proxy is resolved (#20)
    -- removed cache manager since it is no longer used
    -- updated member fields to use appropriate enums

--- src/app/auth/auth.resolver.ts
+++ src/app/auth/auth.resolver.ts
@@ -4,22 +4,22 @@ import { Auth } from "./models/auth.model";
 import { Token } from "./models/token.model";
 import { LoginInput } from "./dto/login.input";
 import { RefreshTokenInput } from "./dto/refresh-token.input";
-import { UseGuards } from "@nestjs/common";
-import { GqlThrottlerGuard } from "src/common/throttling/GqlThrottlerGuard";
-import { Throttle } from "@nestjs/throttler";
+// import { UseGuards } from "@nestjs/common";
+// import { GqlThrottlerGuard } from "src/common/throttling/GqlThrottlerGuard";
+// import { Throttle } from "@nestjs/throttler";
 import { ResetPasswordInput } from "./dto/reset-password.input";
 import { ResetPasswordInitiateInput } from "./dto/reset-password-initiate.input";
 import ResponseStatus from "src/common/ResponseClasses/ResponseStatus";
 
 // 4 requests per 60 secods
-const AUTH_THROTTLE_RATE = [4, 60];
+// const AUTH_THROTTLE_RATE = [4, 60];
 
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly auth: AuthService) {}
 
-  @Throttle(...AUTH_THROTTLE_RATE)
-  @UseGuards(GqlThrottlerGuard)
+  // @Throttle(...AUTH_THROTTLE_RATE)
+  // @UseGuards(GqlThrottlerGuard)
   @Mutation(() => Auth)
   async login(@Args("data") { userName, password }: LoginInput) {
     const { accessToken, refreshToken } = await this.auth.login(
@@ -32,8 +32,8 @@ export class AuthResolver {
     };
   }
 
-  @Throttle(...AUTH_THROTTLE_RATE)
-  @UseGuards(GqlThrottlerGuard)
+  // @Throttle(...AUTH_THROTTLE_RATE)
+  // @UseGuards(GqlThrottlerGuard)
   @Mutation(() => ResponseStatus)
   async initiateResetPassword(
     @Args("resetPasswordInitiateInput")
@@ -44,8 +44,8 @@ export class AuthResolver {
     return new ResponseStatus("Completed");
   }
 
-  @Throttle(...AUTH_THROTTLE_RATE)
-  @UseGuards(GqlThrottlerGuard)
+  // @Throttle(...AUTH_THROTTLE_RATE)
+  // @UseGuards(GqlThrottlerGuard)
   @Mutation(() => ResponseStatus)
   async resetPassword(
     @Args("resetPasswordInput") resetPasswordInput: ResetPasswordInput
